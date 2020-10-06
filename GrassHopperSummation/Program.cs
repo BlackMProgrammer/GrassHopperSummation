776ee6336743b25040cbaa3c48ec7f8684740419
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-10-06 11:33:35 -0400

    Add project files.

---
+++ GrassHopperSummation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GrassHopperSummation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumation(22);
+            
+        }
+
+        public static int Sumation(int num) 
+        {
+            int i = 0;
+            int var = 0;
+            while ( i <= num)
+            {
+
+                var += i;
+                Console.WriteLine(var);
+                i++;
+            }
+
+            return var;
+        }
+
+    }
+}
